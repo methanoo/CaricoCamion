77f2d7a2a5279afcde8128e9d836234d8120cbe6
methano_ <98732397+methanoo@users.noreply.github.com>
2024-03-24 17:56:57 +0100

    WOO first official release
    yeee

--- script.js
+++ script.js
@@ -1,39 +1,62 @@
 let carichi = [];
 meno=false;
 canc=false;
+ao=false;
+counter=0;
+menobello=false;
+prior=false;
+larghezzaacolli=0;
+lunghezzaacolli=0;
+indice = new Array();
+
+
 function aggiungiCarico() {
     const lunghezza = parseInt(document.getElementById('lunghezza').value);
     const larghezza = parseInt(document.getElementById('larghezza').value);
     const priorita = parseInt(document.getElementById('priorita').value);
     const tipoOnda = document.getElementById('tipo-onda').value;
     const quantita = parseInt(document.getElementById('quantita').value);
-    
-    if(lunghezza!=lunghezza||larghezza!=larghezza||priorita!=priorita||tipoOnda!=tipoOnda||quantita!=quantita){
+lunghezzaacolli= lunghezza;
+larghezzaacolli = larghezza;
+
+    if(lunghezza!=lunghezza||larghezza!=larghezza||tipoOnda=="onda"||quantita!=quantita){
 alert("ao completa")
+ao=true;
     }else{
+        if(priorita==99){
+           prior=true;
+        }
         const carico = { lunghezza, larghezza, priorita, tipoOnda, quantita, caricati: 0, colli: [] }; 
         carichi.push(carico);
         mostraCarichi();
     }
 
+   
+}
 
+function svuotaCarico(){
+if(ao==true){
+
+}else{
+    document.getElementById("prova").reset();
+}
+ao=false;
 }
 
 function mostraCronologiaColli(index) {
     const cronologiaColliContainer = document.getElementById('colli-lista');
-    cronologiaColliContainer.innerHTML = '';
-    
+    cronologiaColliContainer.innerHTML = `<b>Misura: ${lunghezzaacolli}x${larghezzaacolli}</b>`; 
     const cronologiaColli = carichi[index].colli;
+
     cronologiaColli.forEach((quantitaCaricata, idx) => {
         const li = document.createElement('li');
-        li.textContent = `Carico ${idx + 1}: ${quantitaCaricata}`;
+        li.textContent = `\n Carico ${idx + 1}: ${quantitaCaricata}`;
         cronologiaColliContainer.appendChild(li);
     });
 }
 function svuotacolli(index){
     const cronologiaColliContainer = document.getElementById('colli-lista');
     cronologiaColliContainer.innerHTML = '';
-    
     const cronologiaColli = carichi[index].colli;
     cronologiaColli.forEach((quantitaCaricata, idx) => {
         const li = document.createElement('li');
@@ -55,6 +78,7 @@ function ordinaPerLunghezza() {
     mostraCarichi(); 
 }
 function apriTastierino(index) {
+    indice[index]=0;
     const carico = carichi[index];
 if(parseInt(document.getElementById('carico_per').value)==parseInt(document.getElementById('carico_per').value)){
    for(p=0;p<parseInt(document.getElementById('carico_per').value);p++){
@@ -62,20 +86,27 @@ if(parseInt(document.getElementById('carico_per').value)==parseInt(document.getE
     carico.colli.push(parseInt(document.getElementById('carico_input').value));
     meno = false;
    }
-   mostraCarichi();
+  
+    mostraCarichi();
+
+
+
    document.getElementById("carico_prova").reset();
-}else{
+}
     if (meno==true) {
         if (confirm("Scarti, Confermi?")) {
             carico.caricati = -(parseInt(document.getElementById('carico_input').value)) + carico.caricati;
             carico.colli.push(-(parseInt(document.getElementById('carico_input').value)));
             document.getElementById("carico_prova").reset();
-         
-            mostraCarichi();
             meno = false;
-        }       
+            counter=counter+1;
+            indice[index]=indice[index]-counter;
+        }else{
+            indice[index]=indice[index]-counter
+        }
 
-        
+    }else{
+        indice[index]=indice[index]-counter
     }
     if (canc==true) {
         event.preventDefault();
@@ -86,16 +117,22 @@ if(parseInt(document.getElementById('carico_per').value)==parseInt(document.getE
         } 
        
       if (parseInt(document.getElementById('carico_input').value)==parseInt(document.getElementById('carico_input').value)) {
+        if(carico.priorita==99){
+            alert("we no caricare")
+            document.getElementById("carico_prova").reset();
+            prior=false;
+               }else{
             carico.caricati = parseInt(document.getElementById('carico_input').value) + carico.caricati;
             carico.colli.push(parseInt(document.getElementById('carico_input').value));
             document.getElementById("carico_prova").reset();
             meno = false;
             mostraCarichi();
+               }
         }
         
     }
-    }
     
+
   
     function confermaEliminazione(index) {
         event.preventDefault();
@@ -125,9 +162,7 @@ function calcolaParziale(carico) {
     return carico.quantita - carico.caricati;
 }
 
-function svuotaCarico() {
-    document.getElementById("prova").reset();
-}
+
 function aggiornaStileCarico(index) {
     const carico = carichi[index];
     const parziale = calcolaParziale(carico);
@@ -163,28 +198,47 @@ function mostraCarichi() {
     
         li.classList.add('carico');
 
+      if((carico.colli.length+indice[index])!=(carico.colli.length+indice[index])){
+  indice[index]=0;
+      }
 
-
-    li.innerHTML = ` <u> ${carico.priorita})</u> 
+if(carico.priorita!=carico.priorita){
+carico.priorita=50;
+}
+    li.innerHTML = ` <span style = "vertical-align: middle;"><u> ${carico.priorita}</u>)  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
     <b> ${carico.lunghezza} x ${carico.larghezza} </b>
-    Qt. <b>${carico.quantita}</b>
-    In: <b style="color:red;"><u>${carico.caricati}</u></b> 
-    Rim: <b>${calcolaParziale(carico)}</b> 
-    Col: <b>${carico.colli.length}</b>
-    ${carico.tipoOnda} `;
+    &nbsp; Qt. <b>${carico.quantita}</b>
+    &nbsp; Caricati: <b style="color:red;"><u>${carico.caricati}</u></b> 
+    &ensp; Rim: <b>${calcolaParziale(carico)}</b> 
+    &ensp;Colli: <b>${carico.colli.length+indice[index]}</b>
+    &ensp;Onda:${carico.tipoOnda} &ensp;</span>`;
 
     if(carico.caricati>=(carico.quantita)-5){
-li.innerHTML = ` <u> ${carico.priorita})</u> 
-<b> ${carico.lunghezza} x ${carico.larghezza} </b>
-Qt. <b>${carico.quantita}</b>
-In: <b style="color:green;"><u>${carico.caricati}</u></b> 
-Rim: <b>${calcolaParziale(carico)}</b> 
-Col: <b>${carico.colli.length}</b>
-${carico.tipoOnda} `
-    }
+        li.innerHTML = ` <span style = "vertical-align: middle;"><u> ${carico.priorita}</u>)  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+        <b> ${carico.lunghezza} x ${carico.larghezza} </b>
+        &nbsp; Qt. <b>${carico.quantita}</b>
+        &nbsp; Caricati: <b style="color:green;"><u>${carico.caricati}</u></b> 
+        &ensp; Rim: <b>${calcolaParziale(carico)}</b> 
+        &ensp;Colli: <b>${carico.colli.length+indice[index]}</b>
+        &ensp;Onda:${carico.tipoOnda} &ensp;</span>`;
+} 
+if(carico.caricati >= carico.quantita * 1.1){
+    li.innerHTML = ` <span style = "vertical-align: middle;"><u> ${carico.priorita}</u>)  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+        <b> ${carico.lunghezza} x ${carico.larghezza} </b>
+        &nbsp; Qt. <b>${carico.quantita}</b>
+        &nbsp; Caricati: <b style="color:purple;"><u>${carico.caricati}</u></b> 
+        &ensp; Rim: <b>${calcolaParziale(carico)}</b> 
+        &ensp;Colli: <b>${carico.colli.length+indice[index]}</b>
+        &ensp;Onda:${carico.tipoOnda} &ensp;</span>`;
+}
 
-  
 
+carichiLista.appendChild(li);
+const pulsanteCarica = document.createElement('button');
+pulsanteCarica.textContent = ' ';
+pulsanteCarica.className="caricatext";
+pulsanteCarica.onclick = () => apriTastierino(index);
+li.appendChild(pulsanteCarica);
  
         const linkCronologia = document.createElement('a');
         linkCronologia.href = '#';
@@ -193,15 +247,6 @@ ${carico.tipoOnda} `
         linkCronologia.onclick = () => mostraCronologiaColli(index);
         li.appendChild(linkCronologia);
    
-        carichiLista.appendChild(li);
-        const pulsanteCarica = document.createElement('button');
-        pulsanteCarica.textContent = ' ';
-        pulsanteCarica.className="caricatext";
-        pulsanteCarica.onclick = () => apriTastierino(index);
-        li.appendChild(pulsanteCarica);
-        
-
-  
         aggiornaStileCarico(index);
 
         const parziale = calcolaParziale(carico);
@@ -219,6 +264,7 @@ ${carico.tipoOnda} `
 
 function ordinaPerPriorita() {
     carichi.sort((a, b) => a.priorita - b.priorita);
+    alert(a.priorita)
     mostraCarichi();
 }
 
@@ -226,7 +272,5 @@ function aggiornaCarichi() {
     mostraCarichi();
    
 }
-
-
 
 setInterval(aggiornaCarichi, 1000);
